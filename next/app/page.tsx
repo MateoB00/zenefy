1c460c883eda320570d6b691e501f3e9c2e9476b
MateoB91 <mateobianco91100@gmail.com>
2023-06-21 20:57:38 +0200

    FTZen-10:

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,7 +1,22 @@
 'use client'
 
+import * as React from 'react';
+
+import Header from '../components/header/header'
+import FirstBlocAccueil from './accueil/first_bloc';
+import SecondBlocAccueil from './accueil/second_bloc';
+import ThirdBlocAccueil from './accueil/third_bloc';
+import Footer from '../components/footer/footer'
+
 export default function Page() {
+
   return (
-    <p>a</p>
+    <>
+      <Header />
+      <FirstBlocAccueil />
+      <SecondBlocAccueil />
+      <ThirdBlocAccueil />
+      <Footer />
+    </>
   )
 }
